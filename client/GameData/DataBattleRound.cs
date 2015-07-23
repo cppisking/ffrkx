748092dbd86ca8de990e886c621d31bfb156cca1
Fat-Chocobo <earth2dan@msn.com>
2015-07-24 00:49:54 +0100

    Added RM and Potion Drops.
    Added RM and Potion Drops to "Current battle drop anticipation" window.

--- client/GameData/DataBattleRound.cs
+++ client/GameData/DataBattleRound.cs
@@ -16,6 +16,12 @@ namespace FFRKInspector.GameData
         [JsonProperty("round")]
         public uint Index;
 
+        [JsonProperty("drop_materias")]
+        public List<DataMateria> Materias;
+
+        [JsonProperty("drop_item_list")]
+        public List<DataPotion> Potions;
+
         [JsonExtensionData]
         public Dictionary<string, JToken> UnknownValues;
 
@@ -24,11 +30,34 @@ namespace FFRKInspector.GameData
             get
             {
                 foreach (DataEnemy enemy in Enemies)
+                {
                     foreach (DropEvent drop in enemy.Drops)
                     {
                         drop.Round = Index;
                         yield return drop;
                     }
+                }
+                foreach (DataMateria materia in Materias)
+                {
+                    DropEvent dropMateria = new DropEvent();
+                    dropMateria.MateriaName = materia.Name;
+                    dropMateria.Round = Index;
+                    dropMateria.EnemyName = "";
+                    dropMateria.ItemType = DataEnemyDropItem.DropItemType.Materia;
+                    yield return dropMateria;
+                }
+                foreach (DataPotion potion in Potions)
+                {
+                    DropEvent dropPotion = new DropEvent();
+                    if (potion.Type == 21) { dropPotion.PotionName = "Potion"; }
+                    else if (potion.Type == 22) { dropPotion.PotionName = "Hi-Potion"; }
+                    else if (potion.Type == 31) { dropPotion.PotionName = "Ether"; }
+                    else { dropPotion.PotionName = "Unknown Potion"; }
+                    dropPotion.Round = Index;
+                    dropPotion.EnemyName = "";
+                    dropPotion.ItemType = DataEnemyDropItem.DropItemType.Potion;
+                    yield return dropPotion;
+                }
             }
         }
     }
---
+++ client/GameData/DataMateria.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFRKInspector.GameData
+{
+    class DataMateria
+    {
+        [JsonProperty("name")]
+        public string Name;
+
+        [JsonExtensionData]
+        public Dictionary<string, JToken> UnknownValues;
+
+    }
+}
